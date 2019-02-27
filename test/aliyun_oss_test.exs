17b4982d12acec819e6c0fccbf011d37dad8372c
ug0 <ug600@icloud.com>
2019-02-27 15:40:49 +0800

    Add test for README install version check

--- test/aliyun_oss_test.exs
+++ test/aliyun_oss_test.exs
@@ -1,4 +1,22 @@
 defmodule Aliyun.OssTest do
   use ExUnit.Case
   doctest Aliyun.Oss
+
+  test "README install version check" do
+    app = :aliyun_oss
+
+    app_version = "#{Application.spec(app, :vsn)}"
+    readme = File.read!("README.md")
+    [_, readme_versions] = Regex.run(~r/{:#{app}, "(.+)"}/, readme)
+
+    assert Version.match?(
+             app_version,
+           readme_versions
+           ),
+           """
+           Install version constraint in README.md does not match to current app version.
+           Current App Version: #{app_version}
+           Readme Install Versions: #{readme_versions}
+           """
+  end
 end

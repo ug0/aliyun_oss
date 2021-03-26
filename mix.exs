60f1bbf6928da1c7b99eee8325e08ced751ef4b1
ug0 <ug600@icloud.com>
2021-03-26 09:40:05 +0800

    Release v1.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AliyunOss.MixProject do
   def project do
     [
       app: :aliyun_oss,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),

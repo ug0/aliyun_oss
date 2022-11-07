4ec5c2c3fd9fe0309da160313d155c0533c3e0f1
c4710n <c4710n@users.noreply.github.com>
2022-11-07 21:59:08 +0800

    Prepare alternative Config module which will replace existing one

--- lib/aliyun_oss/config.ex
+++ lib/aliyun_oss/config.ex
@@ -8,3 +8,46 @@ defmodule Aliyun.Oss.Config do
     end
   end)
 end
+
+defmodule Aliyun.Oss.ConfigAlt do
+  @enforce_keys [:endpoint, :access_key_id, :access_key_secret]
+  defstruct @enforce_keys
+
+  @type config() :: %{
+          endpoint: String.t(),
+          access_key_id: String.t(),
+          access_key_secret: String.t()
+        }
+
+  @type t :: %__MODULE__{
+          endpoint: String.t(),
+          access_key_id: String.t(),
+          access_key_secret: String.t()
+        }
+
+  @spec new!(config()) :: __MODULE__.t()
+  def new!(config) when is_map(config) do
+    config
+    |> validate_required_keys!()
+    |> as_struct!()
+  end
+
+  defp validate_required_keys!(
+         %{
+           endpoint: endpoint,
+           access_key_id: access_key_id,
+           access_key_secret: access_key_secret
+         } = config
+       )
+       when is_binary(endpoint) and is_binary(access_key_id) and is_binary(access_key_secret) do
+    config
+  end
+
+  defp validate_required_keys!(_config) do
+    raise ArgumentError, "config :endpoint, :access_key_id, :access_key_secret are required"
+  end
+
+  defp as_struct!(config) do
+    struct!(__MODULE__, config)
+  end
+end

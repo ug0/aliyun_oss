cc8d15c8c0687b07825959516a230675eac7cddb
c4710n <c4710n@users.noreply.github.com>
2022-11-22 20:20:21 +0800

    Fix tests

--- test/aliyun_oss/object_test.exs
+++ test/aliyun_oss/object_test.exs
@@ -1,68 +1,61 @@
 defmodule Aliyun.Oss.Client.ObjectTest do
   use ExUnit.Case
 
-  import Mock
   alias Aliyun.Oss.Object
   alias Aliyun.Oss.Config
 
   @endpoint "oss-example.oss-cn-hangzhou.aliyuncs.com"
   @access_key_id "44CF9590006BF252F707"
   @access_key_secret "OtxrzxIsfpFjA7SwPzILwy8Bw21TLhquhboDYROV"
+
+  @config Config.new!(%{
+            endpoint: @endpoint,
+            access_key_id: @access_key_id,
+            access_key_secret: @access_key_secret
+          })
+
   describe "signed_url/5" do
     test "build signed object url for GET" do
-      with_mock Config,
-        endpoint: fn -> @endpoint end,
-        access_key_id: fn -> @access_key_id end,
-        access_key_secret: fn -> @access_key_secret end do
-        assert Object.signed_url("test_bucket", "test_object_key", 1_547_105_286, "GET", %{
-                 "Content-Type" => ""
-               }) ==
-                 "https://test_bucket.oss-example.oss-cn-hangzhou.aliyuncs.com/test_object_key?Expires=1547105286&OSSAccessKeyId=44CF9590006BF252F707&Signature=8bRz6KZY9DF93%2F4whvcyyv3UY4k%3D"
+      assert Object.signed_url(@config, "test_bucket", "test_object_key", 1_547_105_286, "GET", %{
+               "Content-Type" => ""
+             }) ==
+               "https://test_bucket.oss-example.oss-cn-hangzhou.aliyuncs.com/test_object_key?Expires=1547105286&OSSAccessKeyId=44CF9590006BF252F707&Signature=8bRz6KZY9DF93%2F4whvcyyv3UY4k%3D"
 
-        assert Object.signed_url(
-                 "test_bucket",
-                 "test_object_key",
-                 1_547_105_286,
-                 "GET",
-                 %{"Content-Type" => ""},
-                 %{"acl" => nil}
-               ) ==
-                 "https://test_bucket.oss-example.oss-cn-hangzhou.aliyuncs.com/test_object_key?Expires=1547105286&OSSAccessKeyId=44CF9590006BF252F707&Signature=%2FNIaPMkHsNUdIUfwp7e%2BFz%2FvytI%3D&acl="
-      end
+      assert Object.signed_url(
+               @config,
+               "test_bucket",
+               "test_object_key",
+               1_547_105_286,
+               "GET",
+               %{"Content-Type" => ""},
+               %{"acl" => nil}
+             ) ==
+               "https://test_bucket.oss-example.oss-cn-hangzhou.aliyuncs.com/test_object_key?Expires=1547105286&OSSAccessKeyId=44CF9590006BF252F707&Signature=%2FNIaPMkHsNUdIUfwp7e%2BFz%2FvytI%3D&acl="
     end
 
     test "build signed object url for PUT" do
-      with_mock Config,
-        endpoint: fn -> @endpoint end,
-        access_key_id: fn -> @access_key_id end,
-        access_key_secret: fn -> @access_key_secret end do
-        assert Object.signed_url("test_bucket", "test_object_key", 1_547_105_286, "PUT", %{
-                 "Content-Type" => "text/plain"
-               }) ==
-                 "https://test_bucket.oss-example.oss-cn-hangzhou.aliyuncs.com/test_object_key?Expires=1547105286&OSSAccessKeyId=44CF9590006BF252F707&Signature=qEYcrQ8CvOxaqVZZ8Kcw%2B3pf2EA%3D"
+      assert Object.signed_url(@config, "test_bucket", "test_object_key", 1_547_105_286, "PUT", %{
+               "Content-Type" => "text/plain"
+             }) ==
+               "https://test_bucket.oss-example.oss-cn-hangzhou.aliyuncs.com/test_object_key?Expires=1547105286&OSSAccessKeyId=44CF9590006BF252F707&Signature=qEYcrQ8CvOxaqVZZ8Kcw%2B3pf2EA%3D"
 
-        assert Object.signed_url(
-                 "test_bucket",
-                 "test_object_key",
-                 1_547_105_286,
-                 "PUT",
-                 %{"Content-Type" => "text/plain", "x-oss-object-acl" => "private"},
-                 %{"acl" => nil}
-               ) ==
-                 "https://test_bucket.oss-example.oss-cn-hangzhou.aliyuncs.com/test_object_key?Expires=1547105286&OSSAccessKeyId=44CF9590006BF252F707&Signature=ca%2BwDexHaI3vO3LV6qZryoMUKYM%3D&acl="
-      end
+      assert Object.signed_url(
+               @config,
+               "test_bucket",
+               "test_object_key",
+               1_547_105_286,
+               "PUT",
+               %{"Content-Type" => "text/plain", "x-oss-object-acl" => "private"},
+               %{"acl" => nil}
+             ) ==
+               "https://test_bucket.oss-example.oss-cn-hangzhou.aliyuncs.com/test_object_key?Expires=1547105286&OSSAccessKeyId=44CF9590006BF252F707&Signature=ca%2BwDexHaI3vO3LV6qZryoMUKYM%3D&acl="
     end
   end
 
   describe "object_url/2" do
     test "build signed object url for downloading" do
-      with_mock Config,
-        endpoint: fn -> @endpoint end,
-        access_key_id: fn -> @access_key_id end,
-        access_key_secret: fn -> @access_key_secret end do
-        assert Object.object_url("test_bucket", "test_object_key", 1_547_105_286) ==
-                 "https://test_bucket.oss-example.oss-cn-hangzhou.aliyuncs.com/test_object_key?Expires=1547105286&OSSAccessKeyId=44CF9590006BF252F707&Signature=8bRz6KZY9DF93%2F4whvcyyv3UY4k%3D"
-      end
+      assert Object.object_url(@config, "test_bucket", "test_object_key", 1_547_105_286) ==
+               "https://test_bucket.oss-example.oss-cn-hangzhou.aliyuncs.com/test_object_key?Expires=1547105286&OSSAccessKeyId=44CF9590006BF252F707&Signature=8bRz6KZY9DF93%2F4whvcyyv3UY4k%3D"
     end
   end
 
@@ -78,7 +71,7 @@ defmodule Aliyun.Oss.Client.ObjectTest do
         "expiration" => "2013-12-01T12:00:00Z"
       }
 
-      assert Object.sign_post_policy(policy, @access_key_secret) == %{
+      assert Object.sign_post_policy(@config, policy) == %{
                policy:
                  "eyJjb25kaXRpb25zIjpbWyJjb250ZW50LWxlbmd0aC1yYW5nZSIsMCwxMDQ4NTc2MF0seyJidWNrZXQiOiJhaGFoYSJ9LHsiQSI6ImEifSx7ImtleSI6IkFCQyJ9XSwiZXhwaXJhdGlvbiI6IjIwMTMtMTItMDFUMTI6MDA6MDBaIn0=",
                signature: "W835KpLsL6k1/oo28RcsEflB6hw="
